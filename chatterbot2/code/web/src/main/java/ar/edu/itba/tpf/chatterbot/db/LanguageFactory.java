f01afaa7e30afaaf0f7f1eff0b141a9a2a7b1ce0
Desiree Leal <desiree.leal1@ucalgary.ca>
2021-04-18 16:17:17 -0600

    updated language factory

--- chatterbot2/code/web/src/main/java/ar/edu/itba/tpf/chatterbot/db/LanguageFactory.java
+++ chatterbot2/code/web/src/main/java/ar/edu/itba/tpf/chatterbot/db/LanguageFactory.java
@@ -1,6 +1,6 @@
 public class LanguageFactory {
 
-  public String getLanguage(String s) {
+  public StartUpDatabase getLanguage(String s) {
     if (s.charAt(0) == 's' || s.charAt(0) == 'S')
       return new StartUpDatabase("../impl/src/main/resources/impl-beansSP.xml");
     if (s.charAt(0) == 'e' || s.charAt(0) == 'E')
